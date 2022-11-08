a487168e6a931f3d692c5cb2668bf556626c0d64
Patryk <patryk.ciwinski@gmail.com>
2022-11-08 15:07:21 +0100

    list comprehension in saint cities

--- swiete_miasta.py
+++ swiete_miasta.py
@@ -2,10 +2,7 @@ import pandas as pd
 df=pd.read_csv("cities.csv", skipinitialspace = True)
 # print(list(df))
 city_name=df["City"]
-sait_cities_list=[]
-for name in city_name:
-    if name.startswith("Santa ") or name.startswith("Saint ") or name.startswith("San "):
-        sait_cities_list.append(name)
+sait_cities_list=[name for name in city_name if name.startswith("Santa ") or name.startswith("Saint ") or name.startswith("San ")]
 print(len(sait_cities_list))
 
 print(sait_cities_list)
